74b7a28ea11beb2df15e393d944feac5fa569f58
Shawn Harris <shawn.harris3510@gmail.com>
2019-08-07 10:49:25 -0400

    question array possibility

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log("working");
+
+// points
+let points = document.querySelector(".points");
+
+// timer
+
+//questions categories
+let questionsCat1 = {
+    question: "Harry Potter",
+    answers: [
+        ["Rick Riordian", false],
+        ["Cassandra Clare", false],
+        ["JK Rowling", true],
+        ["Laurel Hamilton", false]
+    ]
+};
+
+function clickQuestion() {
+
+   document.getElementById().addEventListener("click", ) 
+}
+
+
+
