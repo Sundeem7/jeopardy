965383a0b761892237888b8f99cc64bb3e251280
Shawn Harris <shawn.harris3510@gmail.com>
2019-08-07 12:27:40 -0400

    display questions on click not working

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
 console.log("working");
 
-// points
-let points = document.querySelector(".points");
+// // points
+// let points = document.querySelector(".points");
 
-// timer
+// // timer
 
-//questions categories
-let questionsCat1 = {
-    question: "Harry Potter",
-    answers: [
-        ["Rick Riordian", false],
-        ["Cassandra Clare", false],
-        ["JK Rowling", true],
-        ["Laurel Hamilton", false]
-    ]
-};
+// //questions categories
+// let questionsCat1 = {
+//     question: "Harry Potter",
+//     answers: [
+//         ["Rick Riordian", false],
+//         ["Cassandra Clare", false],
+//         ["JK Rowling", true],
+//         ["Laurel Hamilton", false]
+//     ]
+// };
 
+// click .points(any square with a points value) to make question appear
 function clickQuestion() {
-let questionsD = document.querySelectorAll(".questionDisplay");
-
-   document.querySelectorAll(".points").addEventListener("click", event)
-    if (questionDisplay.style.display === "none") {
-        questionDisplay.style.display === "block";
-    } else {
-        questionDisplay.style.display === "none";
+    // declare .points
+    let pointsQuestions = document.querySelectorAll(".points");
+    // create array for .points
+    for (let i = 0; i < pointsQuestions.length; i++) {
+        // add event listener to all .points
+        pointsQuestions[i].addEventListener("click", (event) => {
+            // declare .questionDisplay
+            let questionDisplay = document.querySelector(".questionDisplay");
+            console.log(questionDisplay)
+            // display on click
+            if (questionDisplay.style.display === "block") {
+                questionDisplay.style.display === "none";
+            // } else {
+            //     questionDisplay.style.display === "none";
+            }
+        })
     }
 }
-
-
-
+clickQuestion()
