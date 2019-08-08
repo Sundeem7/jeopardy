25016c1b38ce2a5600390b14db1e1e43e61656ee
Shawn Harris <shawn.harris3510@gmail.com>
2019-08-07 20:27:32 -0400

    timer still not working

--- js/app.js
+++ js/app.js
@@ -4,12 +4,34 @@ console.log("working");
 // let points = document.querySelector(".points");
 
 // // timer
-function seconds() {
-    let timer = document.querySelector(".timer").innerHTML
+let seconds = 0
+let mins = 0
+let hours = 0
 
-    
+function timer() {
+    // let timer = document.querySelector(".timer").innerHTML
+    // seconds++
+    // console.log(seconds);
+    if (seconds < 60){
+        seconds++
+        console.log(seconds);
+    } mins++
+        if (mins < 60) {
+            console.log(mins);
+            hours++
+        }
+
   }
-  setTimeout(seconds, 1000);
+  setTimeout(timer, 1000);
+//   timer();
+//   timer();
+//   timer();
+//   timer();
+//   timer();
+//   timer();
+//   timer();
+//   timer();
+//   timer();
 
 
 // click .points(any square with a points value) to make question appear
