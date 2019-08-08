72fef2ec9456e64dfbe255068042e4cbefed366e
Shawn Harris <shawn.harris3510@gmail.com>
2019-08-07 23:09:29 -0400

    timer working, still needs to appear in timer box

--- js/app.js
+++ js/app.js
@@ -4,34 +4,52 @@ console.log("working");
 // let points = document.querySelector(".points");
 
 // // timer
-let seconds = 0
+let secs = 1
 let mins = 0
 let hours = 0
 
+// function timer() {
+//     // for (seconds = 0; seconds < 60; seconds++) {
+//     console.log(seconds)
+//     // }
+// }
+// setTimeout(timer, 1000);
 function timer() {
-    // let timer = document.querySelector(".timer").innerHTML
-    // seconds++
-    // console.log(seconds);
-    if (seconds < 60){
-        seconds++
-        console.log(seconds);
-    } mins++
-        if (mins < 60) {
-            console.log(mins);
-            hours++
-        }
+    window.setTimeout(timer, 1000)
+if (secs <= 59) {
+    console.log(hours + ":" + mins + ":" + secs)
+    secs++
+} else if (secs = 59) {
+    mins++
+    secs = 0;
+    console.log(hours + ":" + mins + ":" + secs);
+    secs = 1;    
+} else if (mins = 59) {
+    hours++
+    mins = 0;
+    console.log(hours + ":" + mins + ":" + secs)
+}
+}
+// } if (secs = 60) {
+//     console.log(mins)
+//     }
+//     mins++
 
-  }
-  setTimeout(timer, 1000);
-//   timer();
-//   timer();
-//   timer();
-//   timer();
-//   timer();
-//   timer();
-//   timer();
-//   timer();
-//   timer();
+
+timer();
+
+// function hello() {
+//   console.log("Hello");
+//   window.setTimeout(hello, 5000);
+// }
+// hello();
+
+
+// for (seconds = 0; seconds < 60; seconds++) {
+//     timer(seconds)
+// console.log(seconds)
+// }
+
 
 
 // click .points(any square with a points value) to make question appear
